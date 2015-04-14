b6a98111c981f8ca684d69dd19b6e689b242b155
Beatriz Santana Fagundes Souza de Lima <bs2899@columbia.edu>
2015-04-13 23:07:08 -0400

    Making the Flash Card Dynamic

--- public/app.js
+++ public/app.js
@@ -1,1 +1,13 @@
 var app = angular.module('FlashCards', []);
+
+app.controller('FlashCardController', function ($scope) {
+    // We now have access to our scope using $scope.
+    $scope.flashCard = {
+    	question: 'What is Angular?',
+    	answers: [
+        	{ text: 'A front-end framework for great power!', correct: true },
+        	{ text: 'Something lame, who cares, whatever.', correct: false },
+        	{ text: 'Some kind of fish, right?', correct: false }
+    	]
+	};
+});
